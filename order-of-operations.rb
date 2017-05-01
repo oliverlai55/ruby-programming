a2ceb8e5133ad2a485a25cf427b656943424bfc5
oliverlai55 <oliverlai55@gmail.com>
2017-05-01 03:51:42 -0400

    Add notes

--- order-of-operations.rb
+++ order-of-operations.rb
@@ -3,3 +3,16 @@
 #  Parenthesis, Exponents, Multiplication, Division, Addition, Subtraction
 
 p 5 + 15 * 20 - 2 / 6**3 - (3 + 1)
+
+# Float vs Int datatype
+2.0 / 216
+# Make it accurate
+
+# Integer
+1, 2, 3
+
+# Float
+1.2, 3.1, 4.2
+
+# Decimals
+3.34234234524, 3.9832948
