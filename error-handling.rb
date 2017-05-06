8dc00b758ed406edf90c31db20ea94a7f1055898
oliverlai55 <oliverlai55@gmail.com>
2017-05-06 17:28:05 -0400

    Add more notes on error handling

--- error-handling.rb
+++ error-handling.rb
@@ -1,5 +1,28 @@
+# Don't use this syntax
 begin
   puts 8/0
 rescue
   "Rescued the error"
+end
+
+# Now we have a more detailed way of error handling
+begin
+  puts 8/0
+rescue ZeroDivisionError => e
+  puts "Error occured: #{e}"
+end
+
+# Check the diff between StandardError and ZeroDivisionError??
+
+# Custom Error Handling
+def error_logger(e)
+  File.open('error-handling-lessons/error_log.txt', 'a') do |file|
+    file.puts e
+  end
+end
+
+begin
+  puts nil + 10
+rescue StandardError => e
+  error_logger("Error: #{e} at #{Time.now}")
 end
