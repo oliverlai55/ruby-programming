3c0cd159c653248b0123b14709bc72a68b86a8a6
oliverlai55 <oliverlai55@gmail.com>
2017-05-07 14:51:04 -0400

    Finish notes on regex

--- regex.rb
+++ regex.rb
@@ -21,3 +21,45 @@ end
 
 # Example
 p is_valid_email? "jordan@hudgens.com" ? "Valid" : "Invalid"
+
+# IP Address match
+# 172.16.0.0 to 172.31.255.255
+IP_ADDRESS_REGEX = /^((?:(?:^|\.)(?:\d|[1-9]\d|[1-9]\d|1\d{2}|2[0-4]\d|25[0-5])){4})$/
+
+def is_valid_ip_address? ip
+  ip =~ IP_ADDRESS_REGEX
+end
+
+p is_valid_ip_address?("999.16.0.0") ? "Valid" : "Invalid"
+
+# Use Rubular for Regex testing
+
+# Wheel of fortune guessing game
+starting_sentence = "Hi from matching land"
+sentence_array = starting_sentence.split("").map(&:downcase)
+
+# take out empty spaces in the array
+account_count = sentence_array - [" "]
+
+# gsub = global substitution, great for search strings
+final_sentence = starting_sentence.gsub(/[a-zA-Z]/, "_").split("")
+
+while sentence_array.count("") < account_count.count
+  puts "Guess a letter:"
+  guess = gets.downcase.chomp
+
+# finding values in an array
+  if sentence_array.include?(guess)
+
+    # find the position of the array
+    letter_index = sentence_array.find_index(guess)
+
+    # when you find the position in the array, set it to empty string
+    sentence_array[letter_index] = ""
+
+    final_sentence[letter_index] = guess
+    puts "Correct! The sentence is now: #{final_sentence.join}"
+  else
+    puts "Sorry, the letter isn't the right answer, please try again."
+  end
+end
