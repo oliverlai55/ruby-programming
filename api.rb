a01166ad6d2680e74e8b111319ce704cf077a3d1
oliverlai55 <oliverlai55@gmail.com>
2017-05-09 00:23:13 -0400

    Add api notes

--- api.rb
+++ api.rb
@@ -12,3 +12,12 @@ end
 
 edutechional_resty = EdutechionalResty.new
 puts edutechional_resty.posts
+
+response = HTTParty.get('')
+
+puts response.body
+puts response.code
+# figures out if the API is working
+puts response.message
+# HTTP protocal status message
+puts response.headers.inspect
