51818d51bba362e19d654a4e280bd19334111c3f
oliverlai55 <oliverlai55@gmail.com>
2017-05-10 00:30:45 -0400

    Set custom api class

--- api.rb
+++ api.rb
@@ -21,3 +21,25 @@ puts response.code
 puts response.message
 # HTTP protocal status message
 puts response.headers.inspect
+
+# Custom API
+class StackExchange
+  include HTTParty
+  base_uri 'api.stackexchange.com'
+
+  def initialize(service, page)
+    @options = { query: {site: service}}
+  end
+
+  def questions
+    self.class.get('/2.2/questions', @options)
+  end
+
+  def users
+    self.class.get('/2.2/users', @options)
+  end
+end
+
+stack_exchange = StackExchange.new('stackoverflow', 1)
+puts stack_exchange.questions
+puts stack_exchange.users
