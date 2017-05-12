306324dcdb051dff0882e9c3ce62db1ca4edc5c4
oliverlai55 <oliverlai55@gmail.com>
2017-05-11 21:08:12 -0400

    Add notes on algorithm

---
+++ algorithm.rb
@@ -0,0 +1,23 @@
+# Bubble Sort
+def bubble_sort(array)
+  n = array.length
+
+  loop do
+    # [1, 4, 1, 3, 4, 1, 3, 3]
+    swapped = false
+
+    (n-1).times do |i|
+      if array[i] > array[i + 1]
+        array[i], array[i + 1] = array[i + 1], array[i]
+        swapped = true
+      end
+    end
+
+    break if not swapped
+  end
+
+  array
+end
+
+a = [1, 4, 1, 3, 4, 1, 3, 3]
+p bubble_sort(a)
