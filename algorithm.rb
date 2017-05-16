3ce741461c370361d282574afc4ec52029fc1d98
oliverlai55 <oliverlai55@gmail.com>
2017-05-15 21:09:06 -0400

    Add merge sort notes

--- algorithm.rb
+++ algorithm.rb
@@ -43,6 +43,14 @@ p arr.quicksort
 
 # Merge sort
 def merge_sort(list)
+  if list.length <= 1
+    list
+  else
+    mid = (list.length / 2).floor
+    left = merge_sort(list[0..mid - 1])
+    right = merge_sort(list[mid..list.length])
+    merge(left, right)
+  end
 end
 
 def merge(left, right)
@@ -52,6 +60,11 @@ def merge(left, right)
     left
   elsif left.first < right.first
     [left.first] + merge(left[1..left.length], right)
-
+    # merging another array
+  else
+    [right.first] + merge(left, right[1..right.length])
+  end
+end
     # left = [1, 4, 1, 3]
     # right = [100, 101]
+    # newly_merged = [1] + ...[4] +
